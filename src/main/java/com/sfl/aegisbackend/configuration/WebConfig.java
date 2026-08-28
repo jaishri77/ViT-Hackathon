66c4c0aa336189752f4c833991a67a00d15953ec
Myvizhi5 <sairajmyvizhi@gmail.com>
2026-08-29 04:12:48 +0530

    Update WebConfig.java

--- src/main/java/com/sfl/aegisbackend/configuration/WebConfig.java
+++ src/main/java/com/sfl/aegisbackend/configuration/WebConfig.java
@@ -1,6 +1,5 @@
 package com.sfl.aegisbackend.configuration;
 
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://127.0.0.1:3000", "*")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
